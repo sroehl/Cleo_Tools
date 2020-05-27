f4d20a19a42ba9330501afa553407ef73abcf565
jeffwarren <jwarren@cleo.com>
2020-05-27 10:38:07 -0500

    adding template csv files to copy script

--- VersaLex/jsonVersalexRestAPI/Instructions_for_Windows/pbt.sh
+++ VersaLex/jsonVersalexRestAPI/Instructions_for_Windows/pbt.sh
@@ -8,5 +8,8 @@ mvn
 cp /c/git/repo/Cleo_Tools/VersaLex/jsonVersalexRestAPI/tools/HarmonyCSVToJSON/target/HarmonyCSVToJSON*.jar /c/profilebuildertool/HarmonyCSVToJSON.jar
 cp /c/git/repo/Cleo_Tools/VersaLex/jsonVersalexRestAPI/tools/HarmonyCSVToJSON/templates/UserGroup-template.csv /c/profilebuildertool/UserGroup.csv
 cp /c/git/repo/Cleo_Tools/VersaLex/jsonVersalexRestAPI/tools/HarmonyCSVToJSON/templates/UserMailbox-template.csv /c/profilebuildertool/UserMailbox.csv
+cp /c/git/repo/Cleo_Tools/VersaLex/jsonVersalexRestAPI/tools/HarmonyCSVToJSON/templates/AS2Template.csv /c/profilebuildertool/AS2Template.csv
+cp /c/git/repo/Cleo_Tools/VersaLex/jsonVersalexRestAPI/tools/HarmonyCSVToJSON/templates/FTPTemplate.csv /c/profilebuildertool/FTPTemplate.csv
+cp /c/git/repo/Cleo_Tools/VersaLex/jsonVersalexRestAPI/tools/HarmonyCSVToJSON/templates/SFTPTemplate.csv /c/profilebuildertool/SFTPTemplate.csv
 cp /c/git/repo/Cleo_Tools/VersaLex/jsonVersalexRestAPI/tools/HarmonyCSVToJSON/app.properties /c/profilebuildertool/app.properties
 cp /c/git/repo/Cleo_Tools/VersaLex/jsonVersalexRestAPI/target/JsonToVersalexRestAPI*.jar /c/profilebuildertool/JsonToVersalexRestAPI.jar
